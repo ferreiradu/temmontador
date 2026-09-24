cec4385bc52046624ab346ddfeb585b255649803
Carlos Eduardo <eduferreiramga@gmail.com>
2026-09-24 20:05:08 -0300

    Update script.js

--- script.js
+++ script.js
@@ -8,10 +8,19 @@ function buscarCidade(event) {
         .toLowerCase()
         .trim();
 
+    // Remove acentos
     cidade = cidade
         .normalize("NFD")
-        .replace(/[\u0300-\u036f]/g, "")
-        .replace(/\s+/g, "-");
+        .replace(/[\u0300-\u036f]/g, "");
+
+    // Remove o estado no final da busca
+    // Exemplos: -pr, pr, -parana, parana
+    cidade = cidade
+        .replace(/[\s,-]+(pr|parana)$/i, "")
+        .trim();
+
+    // Converte espaços em hífen
+    cidade = cidade.replace(/\s+/g, "-");
 
     window.location.href = cidade + "/index.html";
 }
